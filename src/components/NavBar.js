6b0c3470178b984080036b129a68b969f68eaab7
kennbeckloff <kennbeckloff688@gmail.com>
2022-07-14 16:10:52 +0300

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,10 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  // In the NavBar component, there is an array with three strings representing each link on the page. For each of those strings, create an <a> tag that looks like this:
+  <a href="#home">home</a>
+  <a href="#about">about</a>
+  <a href="#projects">projects</a>
 
   return <nav>{/* display an <a> tag for each link here */}</nav>;
 }
